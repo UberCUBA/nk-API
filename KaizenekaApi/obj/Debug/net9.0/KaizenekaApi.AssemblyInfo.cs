31e343e4551cc3945864ab6a11f248bac8047389
Uber <ubervelazquezramirez@gmail.com>
2025-10-26 16:37:55 -0400

    feat: Add Fly.io configuration file with initial settings

--- KaizenekaApi/obj/Debug/net9.0/KaizenekaApi.AssemblyInfo.cs
+++ KaizenekaApi/obj/Debug/net9.0/KaizenekaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaizenekaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc0d9fa8eb74b440caed5b17b68a752e435da7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be318526e74110172cadd2afef8083bbc1e8d61")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaizenekaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaizenekaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
